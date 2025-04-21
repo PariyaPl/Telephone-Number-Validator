90bd6176e8facdfadc56101e2186212bdac32041
Pariya Palizban <131894233+PariyaPl@users.noreply.github.com>
2025-04-21 20:13:40 +0330

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,146 @@
+body {
+    background: linear-gradient(to right, rgb(19, 33, 34), rgb(43, 76, 78), rgb(20, 35, 36));
+    color: rgb(230,230,230);
+    font-family: 'Quicksand', sans-serif;
+}
+h1 {
+  margin: 1em auto;
+  text-align: center;
+}
+#phone {
+  display: block;
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
+  align-items: center;
+  width: 15em;
+  height: 25em;
+  margin: 1em auto;
+  background-color: rgb(230,230,230);
+  border-radius: 10px;
+}
+#phone-camera {
+  width: 0.5em;
+  height: 0.5em;
+  border-radius: 50px;
+  background-color: rgba(119, 200, 204, 0.623);
+  border: 1px solid rgb(43, 76, 78);
+}
+#phone-screen {
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  flex-direction: column;
+  width: 90%;
+  height: 80%;
+  margin: 0.5em auto;
+  background: linear-gradient(to right, rgb(20,20,20), rgb(32, 57, 58), rgb(20,20,20));
+  border: 1px solid rgb(230,230,230);
+  color: #ffffff;
+}
+h2 {
+  font-size: 1.3rem;
+  margin: 1em auto;
+  text-align: center;
+}
+#user-input {
+  display: block;
+  width: 80%;
+  margin: 2% auto;
+  font-size: 1.5rem;
+  text-align: center;
+  background-color: rgba(48, 47, 47, 0.363);
+  border: 1px solid rgb(230,230,230);
+  color: #ffffff;
+  border-radius: 10px;
+}
+#results-div {
+  display: block;
+  width: 90%;
+  height: 80%;
+  margin: 1em auto;
+  font-size: 1rem;
+  text-align: center;
+  color: #ffffff;
+  overflow-y: auto;
+  white-space: pre-line;
+}
+
+#buttons {
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  width: 90%;
+  height: 10%;
+}
+#check-btn, #clear-btn {
+  margin: 2% auto;
+  height: 90%;
+  width: 40%;
+  font-size: 1rem;
+  background-color: rgb(27, 48, 49);
+  border-color: rgb(43, 76, 78);
+  color: rgb(247, 248, 248);
+}
+#check-btn:hover, #clear-btn:hover {
+  background-color: rgb(3, 5, 5);
+}
+#check-btn:active, #clear-btn:active {
+  background-color: rgb(52, 87, 87);
+}
+
+
+
+@keyframes rotateFlower1 {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes rotateFlower2 {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(-360deg);
+  }
+}
+
+.flower {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  z-index: -1;
+  transform-origin: center center;
+}
+
+.no1 {
+  top: 15rem;
+  left: 15rem;
+  animation: rotateFlower1 60s linear infinite;
+}
+
+.no2 {
+  bottom: 15rem;
+  right: 15rem;
+  animation: rotateFlower2 50s linear infinite;
+}
+
+.petal {
+  position: absolute;
+  width: 80px;
+  height: 80px;
+  background: linear-gradient(to right, rgb(99, 156, 158), rgb(50, 104, 107), 
+  rgb(27, 48, 49), rgba(5, 8, 8, 0.733));
+  border-radius: 120% 0 120% 0;
+  transform-origin: bottom left;
+}
+.petal:nth-child(1) { transform: rotate(0deg)   translateY(-1px); }
+.petal:nth-child(2) { transform: rotate(60deg)  translateY(-1px); }
+.petal:nth-child(3) { transform: rotate(120deg) translateY(-1px); }
+.petal:nth-child(4) { transform: rotate(180deg) translateY(-1px); }
+.petal:nth-child(5) { transform: rotate(240deg) translateY(-1px); }
+.petal:nth-child(6) { transform: rotate(300deg) translateY(-1px); }
